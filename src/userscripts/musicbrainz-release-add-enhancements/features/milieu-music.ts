5d85a54af3c238963b9188f8cabaa81fc062dd70
Raman Sinclair <2878904+arsinclair@users.noreply.github.com>
2026-09-06 20:18:46 +0200

    feat: add Psoma Psi Phi autofill functionality (#15)

--- src/userscripts/musicbrainz-release-add-enhancements/features/milieu-music.ts
+++ src/userscripts/musicbrainz-release-add-enhancements/features/milieu-music.ts
@@ -1,6 +1,5 @@
 import { initLabelAutofill } from "./label-autofill";
 
-const MILIEU_MUSIC_MBID = "30166e7a-d7ca-4b32-9e22-2228958db577";
 const MILIEU_MUSIC_DIGITAL_MBID = "51e69c25-113c-4052-b430-837f9eebb3ac";
 
 export function initMilieuMusicAutofill(): void {
@@ -9,8 +8,14 @@ export function initMilieuMusicAutofill(): void {
         catalogNumberPattern: /\bMilieu\s+Music\s+number\s+([^\s,.;:!?()[\]{}]+)/iu,
         defaultLabelId: MILIEU_MUSIC_DIGITAL_MBID,
         labels: [
-            { id: MILIEU_MUSIC_MBID, names: ["Milieu Music"] },
+            { id: "30166e7a-d7ca-4b32-9e22-2228958db577", names: ["Milieu Music"] },
             { id: MILIEU_MUSIC_DIGITAL_MBID, names: ["Milieu Music Digital"] }
         ]
+    });
+    initLabelAutofill({
+        annotationPattern: /\bPsoma\s+Psi\s+Phi\b/iu,
+        catalogNumberPattern: /\bPsoma\s+Psi\s+Phi\s+number\s+([^\s,.;:!?()[\]{}]+)/iu,
+        defaultLabelId: "2c44071a-be42-4dba-9d76-f1ce5a11157e",
+        labels: [{ id: "2c44071a-be42-4dba-9d76-f1ce5a11157e", names: ["Psøma Psi Phi"] }]
     });
 }
